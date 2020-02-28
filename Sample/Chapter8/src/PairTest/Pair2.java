f66d7d884393c5469513f153ac2c090fe5295562
Clno1 <35722297+Clno1@users.noreply.github.com>
2020-02-28 09:55:55 +0800

    Add files via upload

---
+++ Sample/Chapter8/src/PairTest/Pair2.java
@@ -0,0 +1,32 @@
+package PairTest;
+
+public class Pair2 {
+    public static void main(String[] args) {
+        Pair<Integer> p = new Pair<>(123, 456);
+        int n = add(p);
+        System.out.println(n);
+    }
+
+    static int add(Pair<? extends Number> p) {
+        Number first = p.getFirst();
+        Number last = p.getLast();
+        return p.getFirst().intValue() + p.getFirst().intValue();
+    }
+}
+
+class Pair<T> {
+    private T first;
+    private T last;
+
+    public Pair(T first, T last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+    public T getLast() {
+        return last;
+    }
+}
